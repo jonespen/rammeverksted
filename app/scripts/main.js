3a75521bf8668247cc4b21a0c5f6dd691018bdef
Jon Espen Kvisler <jonespen@gmail.com>
2014-04-06 16:32:42 +0200

    masonry stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,37 +1,45 @@
-$(function() {
-  $.fn.lazyLoad = function() {
-    return this.each(function() {
-      var $this = $(this);
-      if($this.data('loaded')){
-        return;
-      }
+// $(function() {
+//   $.fn.lazyLoad = function() {
+//     return this.each(function() {
+//       var $this = $(this);
+//       if($this.data('loaded')){
+//         return;
+//       }
 
-      var $a = $this.find('a'),
-          imageUrl = $a.attr('href'), 
-          $img = $('<img />').attr('src', imageUrl).attr('alt', '');
+//       var $a = $this.find('a'),
+//           imageUrl = $a.attr('href'), 
+//           $img = $('<img />').attr('src', imageUrl).attr('alt', '');
 
-      $a.remove();
-      $this.append($img).data('loaded', true);
+//       $a.remove();
+//       $this.append($img).data('loaded', true);
       
-    });
-  }
-  var $htmlNbody = $('html, body'),
-      $slides = $('#slides');
+//     });
+//   }
+//   var $htmlNbody = $('html, body'),
+//       $slides = $('#slides');
 
-  $(document).on('animating.slides', function(e, ss) {
-    if(ss.upcoming_slide !== 0){
-      $htmlNbody.animate({
-          scrollTop: $slides.offset().top
-      }, 400);
-    }
-    //$slides.find('li').eq(ss.upcoming_slide+1).lazyLoad();
-  });
+//   $(document).on('animating.slides', function(e, ss) {
+//     if(ss.upcoming_slide !== 0){
+//       $htmlNbody.animate({
+//           scrollTop: $slides.offset().top
+//       }, 400);
+//     }
+//     //$slides.find('li').eq(ss.upcoming_slide+1).lazyLoad();
+//   });
 
-	$slides.superslides({
-		slide_easing: 'easeInOutCubic',
-		slide_speed: 800,
-		pagination: true,
-		hashchange: false,
-		scrollable: true
-	});
+// 	// $slides.superslides({
+// 	// 	slide_easing: 'easeInOutCubic',
+// 	// 	slide_speed: 800,
+// 	// 	pagination: true,
+// 	// 	hashchange: false,
+// 	// 	scrollable: true
+// 	// });
+
+
+// });
+
+var container = document.getElementById('masonry');
+var msnry = new Masonry( container, {
+  columnWidth: '.grid-sizer',
+  itemSelector: '.item'
 });
